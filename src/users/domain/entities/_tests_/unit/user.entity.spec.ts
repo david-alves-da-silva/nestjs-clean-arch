2cbfcfb024285af1059c7dfd2fbb11eda503bef8
david-alves-da-silva <212390alves@gmail.com>
2024-01-04 12:57:14 +0100

    Adding entity validation error

--- src/users/domain/entities/_tests_/unit/user.entity.spec.ts
+++ src/users/domain/entities/_tests_/unit/user.entity.spec.ts
@@ -4,10 +4,12 @@ describe('UserEntity unit tests', () => {
   let props: UserProps;
   let sut: UserEntity;
   beforeEach(() => {
+    UserEntity.validate = jest.fn();
     props = UserDataBuilder({});
     sut = new UserEntity(props);
   });
   it('Constructor method', () => {
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.name).toEqual(props.name);
     expect(sut.props.email).toEqual(props.email);
     expect(sut.props.password).toEqual(props.password);
@@ -44,10 +46,12 @@ describe('UserEntity unit tests', () => {
   });
   it('Should update a user', () => {
     sut.update('other name');
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.name).toEqual('other name');
   });
   it('Should update the password field', () => {
     sut.updatePassword('other password');
+    expect(UserEntity.validate).toHaveBeenCalled();
     expect(sut.props.password).toEqual('other password');
   });
 });
