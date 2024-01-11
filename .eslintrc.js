d8ad0553b32bcc60c274facf5a14e3d6283d7f3f
david-alves-da-silva <212390alves@gmail.com>
2024-01-11 17:47:44 +0100

    Adding SearchParams class - part 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
   rules: {
+    '@typescript-eslint/semi': ['off'],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -26,4 +27,4 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-namespace': 'off',
   },
-};
+}
