14a291f2c51b535469d49f11633a4c81f1ee818f
david-alves-da-silva <212390alves@gmail.com>
2024-01-16 17:34:10 +0100

    EnvConfig export

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
-import { EnvConfiggitService } from './add/shared/infrastructure/env-configgit/env-configgit.service';
-import { UsersModule } from './users/infrastructure/users.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
+import { EnvConfiggitService } from './users/infrastructure/env-configgit/env-configgit.service'
+import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
   imports: [EnvConfigModule, UsersModule],
--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,10 +1,12 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { EnvConfigService } from './env-config.service';
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
-import { join } from 'node:path';
+import { DynamicModule, Module } from '@nestjs/common'
+import { EnvConfigService } from './env-config.service'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
+import { join } from 'node:path'
 
 @Module({
+  imports: [ConfigModule],
   providers: [EnvConfigService],
+  exports: [EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
@@ -13,6 +15,6 @@ export class EnvConfigModule extends ConfigModule {
       envFilePath: [
         join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
       ],
-    });
+    })
   }
 }
