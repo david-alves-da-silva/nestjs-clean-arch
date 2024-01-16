032fcc58c5c57cf03fa523e597b79a3eb56667e6
david-alves-da-silva <212390alves@gmail.com>
2024-01-16 15:51:05 +0100

    Adding DeleteUserUseCase

--- src/shared/domain/repositories/in-memory.repository.ts
+++ src/shared/domain/repositories/in-memory.repository.ts
@@ -1,36 +1,36 @@
-import { Entity } from '../entities/entity';
-import { NotFoundError } from '../errors/not-found-error';
-import { RepositoryInterface } from './repository-contracts';
+import { Entity } from '../entities/entity'
+import { NotFoundError } from '../errors/not-found-error'
+import { RepositoryInterface } from './repository-contracts'
 
 export abstract class InMemoryRepository<E extends Entity>
   implements RepositoryInterface<E>
 {
-  items: E[] = [];
+  items: E[] = []
   async insert(entity: E): Promise<void> {
-    this.items.push(entity);
+    this.items.push(entity)
   }
   async findById(id: string): Promise<E> {
-    return this._get(id);
+    return this._get(id)
   }
   async findAll(): Promise<E[]> {
-    return this.items;
+    return this.items
   }
   async update(entity: E): Promise<void> {
-    await this._get(entity.id);
-    const index = this.items.findIndex(item => item.id === entity.id);
-    this.items[index] = entity;
+    await this._get(entity.id)
+    const index = this.items.findIndex(item => item.id === entity.id)
+    this.items[index] = entity
   }
   async delete(id: string): Promise<void> {
-    await this._get(id);
-    const index = this.items.findIndex(item => item.id === id);
-    this.items.splice(index, 1);
+    await this._get(id)
+    const index = this.items.findIndex(item => item.id === id)
+    this.items.splice(index, 1)
   }
   protected async _get(id: string): Promise<E> {
-    const _id = `${id}`;
-    const entity = this.items.find(item => item.id === id);
+    const _id = `${id}`
+    const entity = this.items.find(item => item.id === id)
     if (!entity) {
-      throw new NotFoundError('Entity not found');
+      throw new NotFoundError('Entity not found')
     }
-    return entity;
+    return entity
   }
 }
