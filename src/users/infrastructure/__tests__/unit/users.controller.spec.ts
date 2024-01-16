0e21b61f707a60a72210feb4fab90faaf1b9ee06
david-alves-da-silva <212390alves@gmail.com>
2024-01-16 20:05:21 +0100

    UsersController unit tests - findOne method

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -8,6 +8,7 @@ import { UpdateUserUseCase } from '@/users/application/usecasses/update-user.use
 import { UpdateUserDto } from '../../dtos/update-user.dto'
 import { UpdatePasswordUseCase } from '@/users/application/usecasses/update-password.usecase'
 import { UpdatePasswordDto } from '../../dtos/update-password.dto'
+import { GetUserUseCase } from '@/users/application/usecasses/getuser.usecase'
 
 describe('UsersController unit tests', () => {
   let sut: UsersController
@@ -97,5 +98,15 @@ describe('UsersController unit tests', () => {
     const result = await sut.remove(id)
     // expect(output).toStrictEqual(result)
     expect(mockDeleteUserUseCase.execute).toHaveBeenCalledWith({ id })
+  })
+  it('should gets a user', async () => {
+    const output: GetUserUseCase.Output = props
+    const mockGetUserUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['getUserUseCase'] = mockGetUserUseCase as any
+    const result = await sut.findOne(id)
+    // expect(output).toStrictEqual(result)
+    expect(mockGetUserUseCase.execute).toHaveBeenCalledWith({ id })
   })
 })
