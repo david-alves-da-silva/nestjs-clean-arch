578b646e06c6fbcbc3a6f81fc6b5d3cc19e80f8f
david-alves-da-silva <212390alves@gmail.com>
2024-01-16 11:22:08 +0100

    Refactoring SignupUseCase

--- src/users/application/usecasses/signup.usecase.ts
+++ src/users/application/usecasses/signup.usecase.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { BadRequestError } from '../errors/bad-request-errors'
 import { UserEntity } from '@/users/domain/entities/user.entity'
+import { HashProvider } from '@/shared/application/providers/hash-provider'
 
 export namespace SignupUseCase {
   export type Input = {
@@ -16,14 +17,20 @@ export namespace SignupUseCase {
     createdAt: Date
   }
   export class UseCase {
-    constructor(private userRepository: UserRepository.Repository) {}
+    constructor(
+      private userRepository: UserRepository.Repository,
+      private hashProvider: HashProvider,
+    ) {}
     async execute(input: Input): Promise<Output> {
       const { email, name, password } = input
       if (!email || !name || !password) {
         throw new BadRequestError('Input data not provided')
       }
       await this.userRepository.emailExists(email)
-      const entity = new UserEntity(input)
+      const hashPassword = await this.hashProvider.generateHash(password)
+      const entity = new UserEntity(
+        Object.assign(input, { password: hashPassword }),
+      )
       await this.userRepository.insert(entity)
       return entity.toJSON()
     }
