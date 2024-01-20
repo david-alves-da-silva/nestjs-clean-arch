2726336baec09755fc42029f3d72a4aacd72a5e8
david-alves-da-silva <212390alves@gmail.com>
2024-01-20 19:02:03 +0100

    Refactor UserEntity unit tests

--- src/users/domain/entities/_tests_/unit/user.entity.spec.ts
+++ src/users/domain/entities/_tests_/unit/user.entity.spec.ts
@@ -1,57 +1,57 @@
-import { UserEntity, UserProps } from '../../user.entity';
-import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { UserEntity, UserProps } from '../../user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 describe('UserEntity unit tests', () => {
-  let props: UserProps;
-  let sut: UserEntity;
+  let props: UserProps
+  let sut: UserEntity
   beforeEach(() => {
-    UserEntity.validate = jest.fn();
-    props = UserDataBuilder({});
-    sut = new UserEntity(props);
-  });
+    UserEntity.validate = jest.fn()
+    props = UserDataBuilder({})
+    sut = new UserEntity(props)
+  })
   it('Constructor method', () => {
-    expect(UserEntity.validate).toHaveBeenCalled();
-    expect(sut.props.name).toEqual(props.name);
-    expect(sut.props.email).toEqual(props.email);
-    expect(sut.props.password).toEqual(props.password);
-    expect(sut.props.createdAt).toBeInstanceOf(Date);
-  });
+    expect(UserEntity.validate).toHaveBeenCalled()
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
   it('Getter of name field', () => {
-    expect(sut.props.name).toBeDefined();
-    expect(sut.props.name).toEqual(props.name);
-    expect(typeof sut.props.name).toBe('string');
-  });
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe('string')
+  })
   it('Setter of name field', () => {
-    sut['name'] = 'other name';
-    expect(sut.props.name).toEqual('other name');
-    expect(typeof sut.props.name).toBe('string');
-  });
+    sut['name'] = 'other name'
+    expect(sut.props.name).toEqual('other name')
+    expect(typeof sut.props.name).toBe('string')
+  })
   it('Getter of email field', () => {
-    expect(sut.props.email).toBeDefined();
-    expect(sut.props.email).toEqual(props.email);
-    expect(typeof sut.props.email).toBe('string');
-  });
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
+    expect(typeof sut.email).toBe('string')
+  })
   it('Getter of password field', () => {
-    expect(sut.props.password).toBeDefined();
-    expect(sut.props.password).toEqual(props.password);
-    expect(typeof sut.props.password).toBe('string');
-  });
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
+    expect(typeof sut.password).toBe('string')
+  })
   it('Setter of password field', () => {
-    sut['password'] = 'other password';
-    expect(sut.props.password).toEqual('other password');
-    expect(typeof sut.props.password).toBe('string');
-  });
+    sut['password'] = 'other password'
+    expect(sut.props.password).toEqual('other password')
+    expect(typeof sut.props.password).toBe('string')
+  })
   it('Getter of createdAt field', () => {
-    expect(sut.props.createdAt).toBeDefined();
-    expect(sut.props.createdAt).toBeInstanceOf(Date);
-  });
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
+  })
   it('Should update a user', () => {
-    sut.update('other name');
-    expect(UserEntity.validate).toHaveBeenCalled();
-    expect(sut.props.name).toEqual('other name');
-  });
+    sut.update('other name')
+    expect(UserEntity.validate).toHaveBeenCalled()
+    expect(sut.props.name).toEqual('other name')
+  })
   it('Should update the password field', () => {
-    sut.updatePassword('other password');
-    expect(UserEntity.validate).toHaveBeenCalled();
-    expect(sut.props.password).toEqual('other password');
-  });
-});
+    sut.updatePassword('other password')
+    expect(UserEntity.validate).toHaveBeenCalled()
+    expect(sut.props.password).toEqual('other password')
+  })
+})
