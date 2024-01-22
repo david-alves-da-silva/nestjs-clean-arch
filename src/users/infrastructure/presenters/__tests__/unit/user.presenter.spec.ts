28fd3c20bd160c15e2cd8937579e99e9507e1f7f
david-alves-da-silva <212390alves@gmail.com>
2024-01-22 15:00:04 +0100

    UserPresenter class unit tests - part 2

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from 'class-transformer'
 import { UserPresenter } from '../../user.presenter'
 
 describe('UserPresenter unit tests', () => {
@@ -9,13 +10,25 @@ describe('UserPresenter unit tests', () => {
     password: 'fake',
     createdAt,
   }
+  let sut: UserPresenter
+  beforeEach(() => {
+    sut = new UserPresenter(props)
+  })
   describe('constructor', () => {
     it('should be defined', () => {
-      const sut = new UserPresenter(props)
       expect(sut.id).toEqual(props.id)
       expect(sut.name).toEqual(props.name)
       expect(sut.email).toEqual(props.email)
       expect(sut.createdAt).toEqual(props.createdAt)
+    })
+  })
+  it('should presenter data', () => {
+    const output = instanceToPlain(sut)
+    expect(output).toStrictEqual({
+      id: 'bf291bce-f98b-46ad-84b6-79dd26c4d927',
+      name: 'test name',
+      email: 'a@a.com',
+      createdAt: createdAt.toISOString(),
     })
   })
 })
