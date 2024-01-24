0c44dbceed21521c018708db2efbd2a35a0ecfd9
david-alves-da-silva <212390alves@gmail.com>
2024-01-24 17:39:35 +0100

    Refactoting the UserPresenter class with the documentation

--- src/users/infrastructure/presenters/user.presenter.ts
+++ src/users/infrastructure/presenters/user.presenter.ts
@@ -1,12 +1,17 @@
 import { CollectionPresenter } from '@/shared/infrastructure/presenters/collection.presenter'
 import { UserOutput } from '@/users/application/dtos/user-output'
 import { ListUsersUseCase } from '@/users/application/usecasses/listusers.usecase'
+import { ApiProperty } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
 
 export class UserPresenter {
+  @ApiProperty({ description: 'Identificação do usuário' })
   id: string
+  @ApiProperty({ description: 'Nome do usuário' })
   name: string
+  @ApiProperty({ description: 'Email do usuário' })
   email: string
+  @ApiProperty({ description: 'Data de criação do usuário' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date
   constructor(output: UserOutput) {
