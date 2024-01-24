acb3751e36b0b4844b582f6cceecc44028da1008
david-alves-da-silva <212390alves@gmail.com>
2024-01-24 18:06:02 +0100

    Adding the documentation in update method

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -171,6 +171,19 @@ export class UsersController {
     const output = await this.getUserUseCase.execute({ id })
     return UsersController.userToResponse(output)
   }
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 422,
+    description: 'Corpo da requisição com dados inválidos',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Id não encontrado',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Acesso não autorizado',
+  })
   @UseGuards(AuthGuard)
   @Put(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
