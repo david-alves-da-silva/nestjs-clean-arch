98c35fbb53aa05c5cde8f6944077cf866cfa92ce
david-alves-da-silva <212390alves@gmail.com>
2024-01-24 18:00:23 +0100

    Adding the documentation in login method

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -83,6 +83,29 @@ export class UsersController {
     const output = await this.signupUseCase.execute(signupDto)
     return UsersController.userToResponse(output)
   }
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        accessToken: {
+          type: 'string',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'Corpo da requisição com dados inválidos',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Email não encontrado',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Credenciais inválidas',
+  })
   @HttpCode(200)
   @Post('login')
   async login(@Body() signinDto: SigninDto) {
